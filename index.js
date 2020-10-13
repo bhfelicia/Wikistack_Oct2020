d97e9bcbe78cd4401e1694f3a7cf2b2497b05fdd
Felicia Heiney <bhfelicia@gmail.com>
2020-10-13 16:51:41 -0400

    added express.static

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const morgan = require("morgan");
 const app = express();
 //logging middleware
 app.use(morgan);
-
+//serving up static files
+app.use(express.static(path.join(__dirname, "./public")))
 //parsing middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
